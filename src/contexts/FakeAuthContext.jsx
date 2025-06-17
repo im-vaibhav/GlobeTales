92548b8be794968624f95ccdb46a7d51fd5321e3
im-vaibahv <vaibhavraj9958@gmail.com>
2025-06-18 03:47:54 +0530

    User changed

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -19,8 +19,8 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Vaibhav",
+  email: "vaibhav@gmail.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
